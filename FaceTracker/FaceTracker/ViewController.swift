535e80a27fdaa0771f27e0a560944d781b50db50
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-24 20:46:25 -0500

    Initial commit 🚀

---
+++ FaceTracker/FaceTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceTracker
+//
+//  Created by Emmanuel Lopez Guerrero on 24/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
